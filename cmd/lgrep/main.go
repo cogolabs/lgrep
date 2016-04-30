1d71185c601597562ebc45404f90a7c364f293ea
Jacob Vallejo <you@jahkeup.com>
2016-04-29 22:36:56 -0400

    Comment out unimplemented features

--- cmd/lgrep/main.go
+++ cmd/lgrep/main.go
@@ -26,8 +26,8 @@ var (
 
 	flagQueryIndex = flag.String("Qi", "", "Index to search")
 	flagQueryDebug = flag.Bool("QD", false, "Debug the query sent to elasticsearch")
-	flagQuerySort  = flag.String("Qs", "@timestamp:desc", "Sort by <field>:<asc|desc> (appended when specified)")
-	flagQueryRegex = flag.String("Qr", "message:^.*$", "Add a regex query to the search (AND'd)")
+	//flagQuerySort  = flag.String("Qs", "timestamp:desc", "Sort by <field>:<asc|desc> (appended when specified)")
+	//flagQueryRegex = flag.String("Qr", "message:^.*$", "Add a regex query to the search (AND'd)")
 
 	flagResultCount         = flag.Int("n", 100, "Number of results to fetch")
 	flagResultFormat        = flag.String("f", DefaultFormat, "Format returned results into text/template format")
@@ -39,6 +39,16 @@ var (
 func usage() {
 	fmt.Fprint(os.Stderr, "lgrep - Logstash grep\n\n")
 	flag.PrintDefaults()
+
+	fmt.Fprint(os.Stderr, `
+
+Reference time: Mon Jan 2 15:04:05 -0700 MST 2006\n
+
+given: { "timestamp": "2016-04-29T13:58:59.420Z" }
+
+{{.timestamp|ftime "15:04"}} => 13:58
+{{.timestamp|ftime "2006-01-02 15:04"}} => 2016-04-29 13:58
+`)
 }
 
 func init() {
