8388ef4b3ef3882b80615ed2277a50863a37ce12
Jacob Vallejo <you@jahkeup.com>
2016-05-06 14:47:52 -0400

    Fixed #9

--- test/issues_test.go
+++ test/issues_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/cogolabs/lgrep"
@@ -21,10 +23,33 @@ func TestIssue9(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(msgs) != 0 {
+	fmt.Println(msgs)
+	if len(msgs) == 0 {
 		t.Fatal("No formatted messages were returned")
 	}
-	if len(msgs[0]) == 0 {
-		t.Fatalf("Formatted message was empty '%s'", msgs[0])
+
+	var (
+		doc      map[string]interface{}
+		expected string
+	)
+	err = json.Unmarshal(*docs[0], &doc)
+	if err != nil {
+		t.Fatalf("Error parsing the json from document: %s", err)
+	}
+	if val, ok := doc["route"]; ok {
+		if route, ok := val.(map[string]interface{}); ok {
+			if val, ok := route["fromdomain"]; ok {
+				if fromdomain, ok := val.(string); ok {
+					expected = fromdomain
+				}
+			}
+		}
+	}
+	if expected == "" {
+		t.Fatal("Not sure what the expected value should be")
+	}
+
+	if msgs[0] != expected {
+		t.Fatalf("Formatted message '%s' was not expected '%s'", msgs[0], expected)
 	}
 }
